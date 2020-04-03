69c8371af7c144593211e6c8ed99b57cfbbe9e29
royckl0163 <60773061+royckl0163@users.noreply.github.com>
2020-04-03 18:00:29 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -9,6 +9,15 @@ enum ON_OFF {
     OFF
 }
 
+enum mode {
+    //% block="AUTO mode"
+    AUTO,
+    //% block="HIGH mode"
+    HIGH,
+    //% block="LOW mode"
+    LOW
+}
+
 //% color=#ff8c1a weight=100 icon="\uf009" block="KUJU"
 namespace kuju {
     //% blockId=onPlug block="set Smart Plug to %_plugVal"
@@ -17,7 +26,17 @@ namespace kuju {
     }
 
     //% blockId=onFan block="set Fans to %_fanVal"
-    export function onFan(_status: ON_OFF): void {
+    export function onFan(_status: mode): void {
+
+    }
+
+    //% blockId=onAir block="set Air conditioner to %_airVal"
+    export function onAir(_status: ON_OFF): void {
+
+    }
+
+    //% blockId=onLight block="set Light to %_lightVal"
+    export function onLight(_status: ON_OFF): void {
 
     }
 }
