9ccda2220a81cbe0cdeafddd793b8e9bd7068287
royckl0163 <60773061+royckl0163@users.noreply.github.com>
2020-04-03 17:54:31 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,29 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * KUJU Block Design
+ */
+
+enum ON_OFF {
+    //% block="ON"
+    ON,
+    //% block="OFF"
+    OFF
+}
+
+//% color=#ff8c1a weight=100 icon="\uf009" block="KUJU"
+namespace kuju {
+    //% blockId=onPlug block="set Smart Plug to %_plugVal"
+    export function onPlug(_status: ON_OFF): void {
+
+    }
+
+    //% blockId=onFan block="set Fans to %_fanVal"
+    export function onFan(_status: ON_OFF): void {
+
+    }
+}
+
+//% color=#ff6680 weight=100 icon="\uf009" block="Devices"
+namespace device {
+
+
+}
